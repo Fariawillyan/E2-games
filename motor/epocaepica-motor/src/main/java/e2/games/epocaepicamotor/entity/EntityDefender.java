c73f7d6cf1671729b9a08f99c17d8158a1085522
fariawillyan <fariawillyan@gmail.com>
2024-03-17 15:57:09 -0300

    new model

---
+++ motor/epocaepica-motor/src/main/java/e2/games/epocaepicamotor/entity/EntityDefender.java
@@ -0,0 +1,15 @@
+package e2.games.epocaepicamotor.entity;
+
+import lombok.Data;
+
+@Data
+public class EntityDefender {
+    private String nameDefender;
+    private String type;
+    private int health;
+    private int attackPower;
+    private int defensePower;
+    private String army;
+    private String nameAttack;
+
+}
