21e8adf7f89c69c1b6d6a722c1597b16a8d3c43b
fariawillyan <fariawillyan@gmail.com>
2024-03-18 12:38:31 -0300

    basic logics + update and insert in DB.

--- motor/epocaepica-motor/src/main/java/e2/games/epocaepicamotor/motor/MotorBattle.java
+++ motor/epocaepica-motor/src/main/java/e2/games/epocaepicamotor/motor/MotorBattle.java
@@ -9,8 +9,6 @@ import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.Optional;
-
 @Service
 @Log4j2
 public class MotorBattle {
@@ -18,21 +16,45 @@ public class MotorBattle {
     @Autowired
     PainelWarDao painelWarDao;
 
-
     public void calculate(EntityAttacker entityAttacker) {
+        final String nameAttack = entityAttacker.getNameAttack();
 
-
-        EntityDefender entityDefender = getDefenderName(entityAttacker.getNameDefense());
+        EntityDefender entityDefender = loadDefenderPlayer(entityAttacker.getNameDefense());
         entityDefender.setNameDefender(entityAttacker.getNameDefense());
 
-        log.info( "MESSAGE IN DB DEFENDER : " + entityDefender);
+        log.info("MESSAGE IN DB DEFENDER : " + entityDefender);
 
-        //... to be continue...
+        final int result = getResultAttack(entityAttacker, entityDefender);
+        entityDefender.setHealth(entityDefender.getHealth() - result);
+        final String nameDefender = entityDefender.getNameDefender();
+        String winner = null;
 
+        if (entityDefender.getHealth() <= 0) {
+
+            log.info("*************** RESULT BATTLE : " + " THE " + nameAttack + " IS VICTORY **************************");
+
+            updateVictory(nameAttack);
+            updateDefeated(nameDefender);
+            winner = nameAttack;
+        } else {
+
+            log.info("*************** RESULT BATTLE : " + " THE " + nameAttack + " IS DEFEATED **************************");
+
+            updateVictory(nameDefender);
+            updateDefeated(nameAttack);
+            winner = nameDefender;
+        }
+
+        createWarTransaction(entityAttacker);
+        createWarResult(nameAttack, nameDefender, winner);
     }
 
-    private EntityDefender getDefenderName(String nameDefender) {
+    private static int getResultAttack(EntityAttacker entityAttacker, EntityDefender entityDefender) {
+        int result = Math.max(0, entityAttacker.getAttackPower()) - entityDefender.getDefensePower();
+        return result;
+    }
 
+    private EntityDefender loadDefenderPlayer(String nameDefender) {
         try {
             return painelWarDao.loadCharacterDao(nameDefender)
                     .orElseThrow(() -> new CustomException("Defender with name " + nameDefender + " not found"));
@@ -41,8 +63,23 @@ public class MotorBattle {
             log.error("Failed to retrieve defender with name {}: {}", nameDefender, e.getMessage());
             throw new RuntimeException("Error retrieving defender", e);
         }
-
     }
 
+    private void createWarTransaction(EntityAttacker entityAttacker) {
+        painelWarDao.createWarTransactionDao(entityAttacker);
+    }
 
+    private void createWarResult(String attacker, String defender, String winner) {
+        painelWarDao.createWarResult(attacker, defender, winner);
+    }
+
+    private void updateVictory(String victory) {
+        painelWarDao.updateCharactersWhenVictoryDao(victory);
+    }
+
+    private void updateDefeated(String defeated) {
+        painelWarDao.updateCharactersWhenDefeatedDao(defeated);
+    }
 }
+
+
