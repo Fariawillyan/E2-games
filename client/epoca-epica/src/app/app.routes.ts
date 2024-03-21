b0cfa6b6aab1b90c5f326c4892778dfc9d5f39d6
fariawillyan <fariawillyan@gmail.com>
2024-03-21 12:08:29 -0300

    add images in home

--- client/epoca-epica/src/app/app.routes.ts
+++ client/epoca-epica/src/app/app.routes.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home/home.component';
+import { LandingPageComponent } from './landing-page/landing-page.component';
 
 export const routes: Routes = [
 
   { path: '', component: HomeComponent },
-  { path: '**', redirectTo: '' }
+  { path: 'landing-page', component: LandingPageComponent }
+
 ];
 
 @NgModule({
